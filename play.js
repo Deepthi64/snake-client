27f5c6128b7a4447870ac14b16bdce5318346f80
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-13 06:39:12 -0400

    created input file

--- play.js
+++ play.js
@@ -1,18 +1,7 @@
 const { connect } = require("./client");
-const { handleUserInput } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-const conn = connect();
+connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-
-  return stdin;
-};
 setupInput();
