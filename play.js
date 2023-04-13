deca44a93ad7c15341a7fca3aece1100be5a0569
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-13 06:26:57 -0400

    setupInput, handleUserInput

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 const { connect } = require("./client");
+const { handleUserInput } = require("./client");
 
 console.log("Connecting ...");
 const conn = connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+setupInput();
